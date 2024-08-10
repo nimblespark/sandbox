af84c29e8add789656e27db51ee94954ab63612c
Alex Carrasco <alex@webchips.net>
2024-08-09 18:27:45 -0600

    Fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { App } from "./App";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
-import { RouterProvider } from "react-router-dom";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { App } from "./App"
+import { Provider } from "react-redux"
+import { store } from "./app/store"
+import { RouterProvider } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
+      <h1>REACT APP</h1>
     </Provider>
   </React.StrictMode>
-);
+)
